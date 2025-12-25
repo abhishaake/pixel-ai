567dfd7315c36d20e02f737cd7e01debd36e9257
Abhishek Verma <abhishekverma0203@gmail.com>
2025-12-25 16:08:32 +0530

    Revert "custom exception"

--- src/main/java/com/av/pixel/exception/IdeogramUnprocessableEntityException.java
+++ src/main/java/com/av/pixel/exception/IdeogramUnprocessableEntityException.java
@@ -1,18 +1,11 @@
 package com.av.pixel.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@NoArgsConstructor
 public class IdeogramUnprocessableEntityException extends RuntimeException{
 
     private String error = "Prompt provided failed safety check due to the inclusion of prohibited content.";
-
-    public IdeogramUnprocessableEntityException(String error) {
-        this.error = error;
-    }
 }
