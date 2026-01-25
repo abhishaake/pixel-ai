c20c2c8453d61eac330bb9ebc68e77ad7bebcce3
Abhishek Verma <abhishekverma0203@gmail.com>
2026-01-25 11:25:55 +0530

    Refactor AsyncConfig to use virtual thread executor

--- src/main/java/com/av/pixel/config/AsyncConfig.java
+++ src/main/java/com/av/pixel/config/AsyncConfig.java
@@ -2,52 +2,22 @@ package com.av.pixel.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
 @Slf4j
 public class AsyncConfig implements AsyncConfigurer {
 
-
-    private final static int corePoolSize = 10;
-    private final static int maxPoolSize = 20;
-    private final static int queueCapacity = 100;
-    private final static int keepAliveSeconds = 30;
-    private final static String threadNamePrefix = "async-";
-    private final static int awaitTerminationSeconds = 30;
-
     @Override
     @Bean(name = "taskExecutor")
-    public TaskExecutor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-
-        executor.setCorePoolSize(corePoolSize);
-        executor.setMaxPoolSize(maxPoolSize);
-        executor.setQueueCapacity(queueCapacity);
-        executor.setKeepAliveSeconds(keepAliveSeconds);
-
-        executor.setThreadNamePrefix(threadNamePrefix);
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.setAwaitTerminationSeconds(awaitTerminationSeconds);
-
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-
-        executor.setAllowCoreThreadTimeOut(true);
-
-        executor.initialize();
-
-        log.info("Async Task Executor initialized with corePoolSize={}, maxPoolSize={}, queueCapacity={}",
-                corePoolSize, maxPoolSize, queueCapacity);
-
-        return executor;
+    public Executor getAsyncExecutor() {
+              return Executors.newVirtualThreadPerTaskExecutor();
     }
 }
